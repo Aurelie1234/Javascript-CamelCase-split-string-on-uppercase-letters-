bde622732dd2ef505c351f48378dd636410d4b1b
Aurelie1234 <115810657+Aurelie1234@users.noreply.github.com>
2023-06-05 16:16:05 +0000

    Create Javascript CamelCase(split string on uppercase letters).js

---
+++ Javascript CamelCase(split string on uppercase letters).js
@@ -0,0 +1,10 @@
+function camelcase(s) {
+    // Write your code here
+    
+// splitting the string on uppercase letters.
+let str  = s.split(/(?=[A-Z])/);
+// counting and returning the words after the split.
+return str.length 
+
+
+}
